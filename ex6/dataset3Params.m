8485da574fa0616f5829ec58ae388987c2048156
Nikita Komarov <nikita@kmrov.ru>
2016-07-24 02:07:49 +0300

    Ex6, parts 1-2 done.

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,19 +15,34 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+Cs = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigmas = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+err_min = inf;
 
+for curC = Cs
+  for cursigma = sigmas
+    fprintf("Training: C = %0.2f, sigma = %0.2f\n", curC, cursigma);
+    model= svmTrain(X, y, curC, @(x1, x2) gaussianKernel(x1, x2, cursigma));
+    predictions = svmPredict(model, Xval);
+    err = mean(double(predictions ~= yval));
+    if err <= err_min
+      err_min = err;
+      C = curC;
+      sigma = cursigma;
+    endif
+  endfor
+endfor
 
-
-
+fprintf("Best values: C = %0.2f, sigma = %0.2f\n", C, sigma);
 
 % =========================================================================
 
